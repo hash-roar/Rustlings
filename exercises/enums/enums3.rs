b2b79e09d034cce5e96174fb1527ace6ce34f27e
hash-roar <1022647354@qq.com>
2022-10-18 01:20:05 +0000

    :tada:

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -2,10 +2,12 @@
 // Address all the TODOs to make the tests pass!
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -38,6 +40,20 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(a, b, c) => {
+                self.color = (a, b, c);
+            }
+            Message::Move(point) => {
+                self.position = point;
+            }
+            Message::Echo(string) => {
+                println!("{}", string);
+            }
+            Message::Quit => {
+                self.quit = true;
+            }
+        }
     }
 }
 
@@ -52,7 +68,7 @@ mod tests {
             position: Point { x: 0, y: 0 },
             color: (0, 0, 0),
         };
-        state.process(Message::ChangeColor((255, 0, 255)));
+        state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
         state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
